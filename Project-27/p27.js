052c1fe41fd172a7a3c9b4dcad1c0fea587a2b87
Prabhat Ranjan <prabhat.rj06@gmail.com>
2025-07-20 17:48:38 +0530

    Vertical Slider (my way)

---
+++ Project-27/p27.js
@@ -0,0 +1,22 @@
+const leftSlides = document.querySelectorAll('.ls-img')
+const rightSlides = document.querySelectorAll('.rs-img')
+const down = document.querySelector('.down-button')
+const up = document.querySelector('.up-button')
+
+let currentLeft = 0
+let currentRight = rightSlides.length - 1
+
+down.addEventListener('click',scroll)
+up.addEventListener('click',scroll)
+
+function scroll(){
+    leftSlides[currentLeft].classList.remove('active')
+    currentLeft = (currentLeft + 1) % leftSlides.length
+    leftSlides[currentLeft].classList.add('active')
+    
+    rightSlides[currentRight].classList.remove('active')
+    currentRight = (currentRight - 1 + rightSlides.length) % rightSlides.length;
+    rightSlides[currentRight].classList.add('active')
+
+}
+
