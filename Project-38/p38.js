c4b595a8006314506f91b9c19add6411c15af65d
Prabhat Ranjan <prabhat.rj06@gmail.com>
2025-08-01 15:05:16 +0530

    Common Background

---
+++ Project-38/p38.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container')
+
+for(let i=0; i<4; i++){
+    for(let j=0; j<4; j++){
+        const box = document.createElement('div')
+        box.classList.add('box')
+        box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+
+        container.appendChild(box)
+    }
+}
